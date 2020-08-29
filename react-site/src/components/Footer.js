37a45cfd757db59efda12f9d64c62ea6a1b9bae7
Jamie Sullivan <jamiesullivan@jamies-mbp.lan>
2020-08-29 14:10:31 -0400

    ready to deploy

--- react-site/src/components/Footer.js
+++ react-site/src/components/Footer.js
@@ -1,30 +1,10 @@
 import React from 'react';
 import './Footer.css';
-import { Button } from './Button';
 import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
     <div className='footer-container'>
-      <section className='footer-subscription'>
-        <p className='footer-subscription-heading'>
-          Join the Adventure newsletter to receive our best vacation deals
-        </p>
-        <p className='footer-subscription-text'>
-          You can unsubscribe at any time.
-        </p>
-        <div className='input-areas'>
-          <form>
-            <input
-              className='footer-input'
-              name='email'
-              type='email'
-              placeholder='Your Email'
-            />
-            <Button buttonStyle='btn--outline'>Subscribe</Button>
-          </form>
-        </div>
-      </section>
       <div class='footer-links'>
         <div className='footer-link-wrapper'>
           <div class='footer-link-items'>
@@ -64,52 +44,34 @@ function Footer() {
         <div class='social-media-wrap'>
           <div class='footer-logo'>
             <Link to='/' className='social-logo'>
-              TRVL
-              <i class='fab fa-typo3' />
+              JME_SULL
+              <i class="fas fa-code"></i> 
             </Link>
           </div>
-          <small class='website-rights'>TRVL © 2020</small>
+          <small class='website-rights'>JAMIE SULLIVAN © 2020</small>
           <div class='social-icons'>
-            <Link
-              class='social-icon-link facebook'
-              to='/'
-              target='_blank'
-              aria-label='Facebook'
+            <a
+              class='social-icon-link github'
+              href='https://github.com/jme-sull/'
+              aria-label='GitHub'
             >
-              <i class='fab fa-facebook-f' />
-            </Link>
-            <Link
-              class='social-icon-link instagram'
-              to='/'
-              target='_blank'
-              aria-label='Instagram'
-            >
-              <i class='fab fa-instagram' />
-            </Link>
-            <Link
-              class='social-icon-link youtube'
-              to='/'
-              target='_blank'
-              aria-label='Youtube'
-            >
-              <i class='fab fa-youtube' />
-            </Link>
-            <Link
-              class='social-icon-link twitter'
-              to='/'
-              target='_blank'
-              aria-label='Twitter'
-            >
-              <i class='fab fa-twitter' />
-            </Link>
-            <Link
-              class='social-icon-link twitter'
-              to='/'
-              target='_blank'
+              <i class="fab fa-github"></i>
+            </a>
+            <a
+              class='social-icon-link linkedin'
+              href='https://www.linkedin.com/in/jamie-sullivan-a6499420/'
               aria-label='LinkedIn'
             >
               <i class='fab fa-linkedin' />
-            </Link>
+            </a>
+            <a
+              class='social-icon-link email'
+              href='mailto:jkcrawshaw@gmail.com'
+              aria-label='Email'
+            >
+              <i class="fas fa-envelope-square"></i>
+            </a>
+            
           </div>
         </div>
       </section>
