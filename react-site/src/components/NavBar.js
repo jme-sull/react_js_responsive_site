c3f4f76fb5b5988ede06b7feccae83ee6a82c140
Jamie Sullivan <jamiesullivan@jamies-mbp.lan>
2020-08-29 13:53:14 -0400

    inital site layout and styling complete

--- react-site/src/components/NavBar.js
+++ react-site/src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import { Button } from './Button';
@@ -14,20 +14,24 @@ function NavBar() {
 
     const showButton = () => {
         if (window.innerWidth <= 960) {
-            setButton(false);
+          setButton(false);
         } else {
-            setButton(true);
+          setButton(true);
         }
-    };
+      };
+    
+    useEffect(() => {
+        showButton();
+      }, []);
 
-    window.addEventListener('reize', showButton);
+    window.addEventListener('resize', showButton);
 
     return (
     <>
         <nav className="navbar">
             <div className="navbar-container">
-                <Link to="/" className="navbar-logo">
-                    JME_SULL <i class="fas fa-code"></i>
+                <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+                JME_SULL <i class="fas fa-code"></i> 
                 </Link>
                 <div className="menu-icon" onClick={handleClick}>
                     <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
@@ -39,7 +43,7 @@ function NavBar() {
                         </Link>
                     </li>
                     <li className='nav-item'>
-                        <Link to='/projets' className="nav-links" onClick={closeMobileMenu}>
+                        <Link to='/projects' className="nav-links" onClick={closeMobileMenu}>
                             Projects
                         </Link>
                     </li>
